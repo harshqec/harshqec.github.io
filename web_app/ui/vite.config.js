98617e1055290728ad76a45bf29cee8325b2e3dd
harsh-quantum <harshg421994@gmail.com>
2026-04-15 00:45:06 +0530

    exp3: chunk-split vendor libs - load 1070->1065ms

--- web_app/ui/vite.config.js
+++ web_app/ui/vite.config.js
@@ -4,5 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './'
+  base: './',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor-react': ['react', 'react-dom'],
+          'vendor-icons': ['lucide-react'],
+          'vendor-axios': ['axios'],
+        }
+      }
+    }
+  }
 })
